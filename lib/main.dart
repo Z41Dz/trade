bd5f991cf83a97884918edf56d7f4ca1203b22de
zzapatam <zaidzapata1@gmail.com>
2023-01-22 07:49:35 -0600

    pantalla finalizada con acceso desde main

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:inicio_2_1/BNavigation/routes.dart';
 
-import 'BNavigation/Bottom_nav.dart';
+import 'Inicio-list/Inicio_list_screen.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  int index = 0;
-  BNavigator? myBNB;
-
-  @override
-  void initState() {
-    myBNB = BNavigator(currentIndex: (i) {
-      setState(() {
-        index = i;
-      });
-    });
-    super.initState();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        /*drawer: const Drawer(),
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),*/
-        bottomNavigationBar: myBNB,
-        body: SafeArea(
-          child: Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Color(0xFF5FB957), Color(0xFF0C69EA)])),
-            child: Routes(index: index),
-          ),
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const InicioList(),
+      },
     );
   }
 }
